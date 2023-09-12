ecf99ef1cda5e2e35ea9a1d657afb987990722bb
rubiomargod <xrubiomargodx@gmail.com>
2023-09-12 16:33:11 -0600

    Acceso a datos

--- AccesoDatos.Tienda/Conexion.cs
+++ AccesoDatos.Tienda/Conexion.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
-namespace AccesoDatos.Tienda
+namespace AccesoDatos.Restaurante
 {
-    public class Class1
+    public class Conexion
     {
+        private MySqlConnection _connection;
+        public Conexion(string Server, string UserID, string Password, string Database, uint Port)
+        {
+            MySqlConnectionStringBuilder CadenaConexion = new MySqlConnectionStringBuilder();
+            CadenaConexion.Server = Server;
+            CadenaConexion.UserID = UserID;
+            CadenaConexion.Password = Password;
+            CadenaConexion.Database = Database;
+            CadenaConexion.Port = Port;
+            _connection = new MySqlConnection(CadenaConexion.ToString());
+        }
+        public void EjecutarConsulta(string Consulta)
+        {
+            try
+            {
+                _connection.Open();
+                using (MySqlCommand command = new MySqlCommand(Consulta, _connection))
+                {
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Consulta Ejecutada Correctamente");
+                }
+                _connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al Ejecutar La Consulta", ex.Message);
+            }
+        }
+        //DataSet guarda varias tablas y Datatable guarda una tabla
+        public DataTable ObtenerDatos(string Consulta)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                _connection.Open();
+                using (MySqlCommand command = new MySqlCommand(Consulta, _connection))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(table);
+                        Console.WriteLine("Consulta Ejecutada Correctamente");
+                    }
+                }
+                _connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al Ejecutar La Consulta", ex.Message);
+            }
+            return table;
+        }
     }
 }
+
--- AccesoDatos.Tienda/ProductosAccesoDatos.cs
+++ AccesoDatos.Tienda/ProductosAccesoDatos.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades.Tienda;
+using AccesoDatos.Restaurante;
 
 namespace AccesoDatos.Tienda
 {
-    internal class ProductosAccesoDatos
+    public class ProductosAccesoDatos
     {
+
     }
 }
